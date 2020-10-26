afbab7397b73c4e9eee395ebb6902010fa30b8ee
지훈 <yamoog@naver.com>
2020-10-26 22:02:01 +0900

    [유틸리 함수 개선] 함수 프로그래밍

--- __PRACTICE__/D08/yamoo9__parcel-sass/src/js/utils.js
+++ __PRACTICE__/D08/yamoo9__parcel-sass/src/js/utils.js
@@ -13,6 +13,13 @@
 // var appHeader = document.querySelector('.appHeader');
 // var homeLink = appHeader.querySelector('.homeLink');
 
+// 완전정복! 함수!!!
+
+// 함수 = 프로시저(절차)
+// 함수 프로그래밍 - 절차형 프로그래밍 - 절차에 맞게 프로그램이 실행
+// 객체 지향 프로그래밍, React
+// JavaScript 언어 (왜??? 함수를 짜서 절차적으로 프로그램이 실행되도록 프로그래밍 사고(생각하는 힘) 하려고)
+
 function findNode(selector, context) {
   // if (!context) { context = document; }
   // context = context ? context : document;
@@ -20,5 +27,24 @@ function findNode(selector, context) {
   return (context || document).querySelector(selector);
 }
 
-var appHeader = findNode('.appHeader');
-var homeLink = findNode('.homeLink', appHeader);
+function on(domNodeOrObject, eventType, eventListener) {
+  domNodeOrObject.addEventListener(eventType, eventListener);
+}
+
+function ready(initializationFunction) {
+  on(window, 'DOMContentLoaded', initializationFunction);
+}
+
+/* -------------------------------------------------------------------------- */
+
+function handleClickBody(e) {
+  var appHeader = findNode('.appHeader');
+  var homeLink = findNode('.homeLink', appHeader);
+  console.log(homeLink);
+}
+
+function init() {
+  on(document.body, 'click', handleClickBody);
+}
+
+ready(init);
