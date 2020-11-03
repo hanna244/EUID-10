eabb1633e3d9032ec7a99c2e69dcc8abdb5e25d6
지훈 <yamoog@naver.com>
2020-11-03 23:15:54 +0900

    [밋업] 실습 자료 추가

---
+++ __MEETUP__/iife-pattern/js/module1.js
@@ -0,0 +1,6 @@
+(function module1(){
+  'use strict';
+
+  var hanna = '김한나'
+  console.log(hanna)
+})();
---
+++ __MEETUP__/web-modules/index.js
@@ -0,0 +1,4 @@
+import { m1 } from './m1.js';
+import { m2 } from './m2.js';
+
+console.log(m1, m2);
