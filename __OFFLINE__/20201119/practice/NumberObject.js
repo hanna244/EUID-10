5876031da058247ee9bdd90f7a73fae931fa8aff
지훈 <yamoog@naver.com>
2020-11-19 14:30:38 +0900

    [실습] 생성자 → 객체

--- __OFFLINE__/20201119/practice/NumberObject.js
+++ __OFFLINE__/20201119/practice/NumberObject.js
@@ -2,6 +2,9 @@
 // 1. 함수 역할
 // 2. 생성자(constructor) 함수 역할(모체) -> 아이(객체) 잉태(생성)
 
+/* -------------------------------------------------------------------------- */
+// 생성자
+
 function Android(brain, alam, camera) {
   this.brain = brain // brain
   this.hasAlamFeature = alam // boolean
@@ -17,3 +20,23 @@ function Camera(maker, zoom) {
   this.maker = maker
   this.zoom = zoom
 }
+
+function 인간(name, age, gender, height) {
+  'use strict'
+
+  this.name = name
+  this.age = age
+  this.gender = gender
+  this.height = height
+}
+
+/* -------------------------------------------------------------------------- */
+// 객체 생성
+
+var samsungCamera = new Camera('samsung', 100)
+var canonCamera = new Camera('canon', 4)
+
+var hannaBrain = new Brain('female', 10000000000)
+var jihoBrain = new Brain('male', 1000000)
+
+var gogleAndroid = new Android(hannaBrain, false, canonCamera)
