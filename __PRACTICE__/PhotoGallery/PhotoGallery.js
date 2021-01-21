9a7ace422d4ee1089047cd6bf951083ba8221912
hanna244 <shhn0509@gmail.com>
2021-01-21 23:32:00 +0900

    [설정] 코드 스타일 적용

--- __PRACTICE__/PhotoGallery/PhotoGallery.js
+++ __PRACTICE__/PhotoGallery/PhotoGallery.js
@@ -1,22 +1,30 @@
-var photoList = ['thumbPhoto-1.jpg', 'thumbPhoto-2.jpg', 'thumbPhoto-3.jpg', 'thumbPhoto-4.jpg']
+var photoList = [
+  'thumbPhoto-1.jpg',
+  'thumbPhoto-2.jpg',
+  'thumbPhoto-3.jpg',
+  'thumbPhoto-4.jpg',
+]
 // photoGalleryButtons [button.photoGallery__button, button.photoGallery__button, button.photoGallery__button, button.photoGallery__button]
 
-var photoGallery = document.querySelector('.photoGallery');
-var photoGalleryButtons = photoGallery.querySelectorAll('button');
-var photoGalleryBigPhoto = photoGallery.querySelector('.photoGallery__bigPhoto');
-var photoGalleryBigPhotoImage = photoGalleryBigPhoto.querySelector('img');
+var photoGallery = document.querySelector('.photoGallery')
+var photoGalleryButtons = photoGallery.querySelectorAll('button')
+var photoGalleryBigPhoto = photoGallery.querySelector('.photoGallery__bigPhoto')
+var photoGalleryBigPhotoImage = photoGalleryBigPhoto.querySelector('img')
 
 function handleChangeBigPhoto(e) {
-    var photoGalleryButtonIndex = e.currentTarget.getAttribute('data-index');
-    var x = Number(photoGalleryButtonIndex) + 1;
-    photoGalleryBigPhotoImage.setAttribute('src', './assets/bigPhoto-' + x + '.jpg')
+  var photoGalleryButtonIndex = e.currentTarget.getAttribute('data-index')
+  var x = Number(photoGalleryButtonIndex) + 1
+  photoGalleryBigPhotoImage.setAttribute(
+    'src',
+    './assets/bigPhoto-' + x + '.jpg'
+  )
 }
-var i = 0;
-var l = photoGalleryButtons.length;
+var i = 0
+var l = photoGalleryButtons.length
 
 while (i < l) {
-    var button = photoGalleryButtons[i];
-    button.setAttribute('data-index', i);
-    button.addEventListener('click', handleChangeBigPhoto);
-    ++i;
+  var button = photoGalleryButtons[i]
+  button.setAttribute('data-index', i)
+  button.addEventListener('click', handleChangeBigPhoto)
+  ++i
 }
