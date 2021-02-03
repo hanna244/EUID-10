006350362ad6a97bd3c1343b1ca4c58ed66460b0
hanna244 <shhn0509@gmail.com>
2021-02-03 19:40:33 +0900

    [실습] create-react-app 실습

---
+++ __PRACTICE__/React_Master/study-react/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> 파일을 저장하면 브라우저 UI가 업데이트
+          됩니다.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+var name = 'hanna'
