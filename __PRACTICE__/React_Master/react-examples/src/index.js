fe2cfa13f39fc5f5a361bcd13241593d4587ca10
hanna244 <shhn0509@gmail.com>
2021-02-05 19:14:22 +0900

    [실습] JSX - 3항식

--- __PRACTICE__/React_Master/react-examples/src/index.js
+++ __PRACTICE__/React_Master/react-examples/src/index.js
@@ -2,41 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-// 조건부 처리할 함수
-function conditionaRendering(count = 0) {
-  // 조건문 switch
-  switch (count) {
-    case 1:
-      return (
-        <ul>
-          <li>조건 문 사용</li>
-          <li>조건 식 (3항식, 논리곱/합 연산자) 활용</li>
-        </ul>
-      )
-      break
-    case 2:
-      return <p>"조건 문 활용" 또는 "조건 식 (3항식, 논리곱/합 연산자) 활용"</p>
-      break
-    default:
-      return (
-        <p>
-          React의 <abbr title="JavaScript Syntax eXtension">JSX</abbr>는
-          JavaScript 객체(React 요소)를 반환합니다.
-        </p>
-      )
-  }
-}
-
-// 숫자를 전달하면 항상 0, 1, 2 중에 하나인 값을 반환하도록 만든다.
-function randomCount(number) {
-  return number % 3
-}
+// 목록 또는 단락을 조건부 렌더링 하기 위한 조건 상태
+let usingList = true
+let usingBorderColor = true
 
 const app = (
   <div className="app">
     <h1>조건부 렌더링</h1>
-    {/* 1-100 중에서 랜덤으로 반환된 실수를 정수로 바꾼다.  */}
-    {conditionaRendering(randomCount(Math.floor(100 * Math.random())))}
+    {/* JSX 내부의 주석 처리 */}
+    {/* 조건에 따라 목록을 렌더링 */}
+    {usingList ? (
+      <ul>
+        <li>조건 문 활용</li>
+        <li
+          className={`bordered ${
+            usingBorderColor ? 'bordered-red' : ''
+          }`.trim()}
+        >
+          조건 식 (3항식, 논리곱/합 연산자) 활용
+        </li>
+      </ul>
+    ) : (
+      <p>"조건 문 활용" 또는 "조건 식 (3항식, 논리곱/합 연산자) 활용"</p>
+    )}
   </div>
 )
 
