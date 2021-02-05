f357c558d68949ae3ad7cc17876036aa10697fc0
hanna244 <shhn0509@gmail.com>
2021-02-05 19:04:06 +0900

    [실습] JSX - 속성 바인딩 (style, className)

--- __PRACTICE__/React_Master/react-examples/src/index.js
+++ __PRACTICE__/React_Master/react-examples/src/index.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
-const bindingContents = {
-  number: 909,
-  string: '콘텐츠 바인딩',
-  howCanIuseJSexpressionInsideJSX() {
-    return 'JavaScript 식(Expression)은 항상 값을 반환함으로 {}안에 바인딩이 가능하다'
-  },
+const state = {
+  appClass: 'app',
 }
 
+const borderColor = 'blue'
+
+const liStyle = { color: '#ea6666', fontWeight: 100 }
+
 const app = (
-  <div className="app">
-    {bindingContents.number + 1} {`${bindingContents.string} hi!!`}
-    <div>{bindingContents.howCanIuseJSexpressionInsideJSX()}</div>
+  <div className={state.appClass}>
+    <h1 className="a11y-hidden">속성 컴파일</h1>
+    <ul>
+      {/* 직접 스타일 추가 */}
+      <li style={{ color: '#ea6666', fontWeight: 900 }}>스타일 속성(인라인)</li>
+      {/* 영역 외부에서 설정한 스타일 추가 */}
+      <li style={liStyle}>스타일 속성(객체)</li>
+      <li className="bordered rounded">클래스 속성</li>
+      <li className={`rounded bordered bordered-${borderColor}`}>
+        클래스 속성(동적 처리)
+      </li>
+    </ul>
   </div>
 )
 
