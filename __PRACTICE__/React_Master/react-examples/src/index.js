b32646b038a0a146d6ac96480c5a72cc7898ac64
hanna244 <shhn0509@gmail.com>
2021-02-05 19:02:58 +0900

    [실습] JSX - 콘텐츠 바인딩/ JavaScript 표현식

--- __PRACTICE__/React_Master/react-examples/src/index.js
+++ __PRACTICE__/React_Master/react-examples/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Data === State
-const state = {
-  appClassName: 'app',
-  appTitleClassName: 'app-title',
-  appHeadlineContents: ['React', '앱'],
+const bindingContents = {
+  number: 909,
+  string: '콘텐츠 바인딩',
+  howCanIuseJSexpressionInsideJSX() {
+    return 'JavaScript 식(Expression)은 항상 값을 반환함으로 {}안에 바인딩이 가능하다'
+  },
 }
 
 const app = (
-  <div className={state.appClassName}>
-    <h1 className={state.appTitleClassName}>
-      <span lang="en">{state.appHeadlineContents[0]}</span>{' '}
-      {state.appHeadlineContents[1]}
-    </h1>
+  <div className="app">
+    {bindingContents.number + 1} {`${bindingContents.string} hi!!`}
+    <div>{bindingContents.howCanIuseJSexpressionInsideJSX()}</div>
   </div>
 )
 
