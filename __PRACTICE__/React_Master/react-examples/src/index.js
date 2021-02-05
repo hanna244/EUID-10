88ae374e21aae99ca4366c61573936a481acb063
hanna244 <shhn0509@gmail.com>
2021-02-05 19:09:01 +0900

    [실습] JSX switch 문

--- __PRACTICE__/React_Master/react-examples/src/index.js
+++ __PRACTICE__/React_Master/react-examples/src/index.js
@@ -3,28 +3,40 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 // 조건부 처리할 함수
-function conditionaRendering(usingList = false) {
-  // 조건문 if, switch
-  if (usingList) {
-    return (
-      <ul>
-        <li>조건 문 사용</li>
-        <li>조건 식 (3항식, 논리곱/합 연산자) 활용</li>
-      </ul>
-    )
-  } else {
-    return <p>"조건 문 활용" 또는 "조건 식 (3항식, 논리곱/합 연산자) 활용"</p>
+function conditionaRendering(count = 0) {
+  // 조건문 switch
+  switch (count) {
+    case 1:
+      return (
+        <ul>
+          <li>조건 문 사용</li>
+          <li>조건 식 (3항식, 논리곱/합 연산자) 활용</li>
+        </ul>
+      )
+      break
+    case 2:
+      return <p>"조건 문 활용" 또는 "조건 식 (3항식, 논리곱/합 연산자) 활용"</p>
+      break
+    default:
+      return (
+        <p>
+          React의 <abbr title="JavaScript Syntax eXtension">JSX</abbr>는
+          JavaScript 객체(React 요소)를 반환합니다.
+        </p>
+      )
   }
 }
 
-function randomBoolean() {
-  return Math.random() < 0.5 ? true : false
+// 숫자를 전달하면 항상 0, 1, 2 중에 하나인 값을 반환하도록 만든다.
+function randomCount(number) {
+  return number % 3
 }
 
 const app = (
   <div className="app">
     <h1>조건부 렌더링</h1>
-    {conditionaRendering(randomBoolean())}
+    {/* 1-100 중에서 랜덤으로 반환된 실수를 정수로 바꾼다.  */}
+    {conditionaRendering(randomCount(Math.floor(100 * Math.random())))}
   </div>
 )
 
