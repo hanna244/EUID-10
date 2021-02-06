d94a9c2b9fc7d9620ac65c33044ac0532c2654ab
hanna244 <shhn0509@gmail.com>
2021-02-06 22:13:27 +0900

    [실습] children 구조 분해 할당

--- __PRACTICE__/React_Master/react-examples/src/Components/Lecture.js
+++ __PRACTICE__/React_Master/react-examples/src/Components/Lecture.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export default class Lecture extends Component {
   render() {
     // const lecturer = this.props.lecturer
-    const { lecturer } = this.props
+    const { lecturer, children } = this.props
     return (
       <li className="lecturers">
         <a href={lecturer.facebook} rel="noreferer nopener">
-          {this.props.children}
+          {children}
         </a>
       </li>
     )
