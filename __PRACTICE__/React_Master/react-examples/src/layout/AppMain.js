89df6e284cf794f9c82a9a052262ac9b9380b8a4
hanna244 <shhn0509@gmail.com>
2021-02-14 18:42:38 +0900

    [실습] AppMain, AppHeader 레이아웃 컴포넌트 생성하여 context 사용

--- __PRACTICE__/React_Master/react-examples/src/layout/AppMain.js
+++ __PRACTICE__/React_Master/react-examples/src/layout/AppMain.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import LectureContext from '../context/LectureContext'
+import Lecturers from '../Components/Lecturers'
 
 export const AppMain = () => {
   return (
     <main className="app-main">
       <h1 className="a11y-hidden">앱 메인 콘텐츠</h1>
-      lectureContext
+      <LectureContext.Consumer>
+        {({ lecturers }) => <Lecturers instructor={lecturers} />}
+      </LectureContext.Consumer>
     </main>
   )
 }
