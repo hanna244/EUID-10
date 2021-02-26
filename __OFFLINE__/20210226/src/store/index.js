c6861281b2cca5c23442a641eccd9c13d37914e8
yamoo9 <yamoog@naver.com>
2021-02-26 12:54:42 +0900

    [수정] logoAnimationRecuer 리듀서 스토어 리듀서로 설정

--- __OFFLINE__/20210226/src/store/index.js
+++ __OFFLINE__/20210226/src/store/index.js
@@ -1,32 +1,9 @@
 // redux에서 createStore 모듈 추출
 import { createStore } from 'redux'
-
-// 초기 상태(initial state)
-const initialState = {
-  animationClass: '',
-}
-
-// 스토어 리듀서(store reducer)
-const storeReducer = (state, action) => {
-  switch (action.type) {
-    default:
-    case 'run 함수 초기 실행':
-      return state
-    case '로고 애니메이션 정지':
-      return {
-        ...state,
-        animationClass: '',
-      }
-    case '로고 애니메이션 실행':
-      return {
-        ...state,
-        animationClass: 'run-animation',
-      }
-  }
-}
+import logoAnimationReducer from './reducers/logoAnimationReducer'
 
 // createStore(reducer, initialState) → store { getState, dispatch, subscribe }
-const store = createStore(storeReducer, initialState)
+const store = createStore(logoAnimationReducer)
 
 // 스토어(store) 내보내기
 export { store }
