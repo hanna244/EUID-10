96ec4ca774c9f10c4962408b23934277469f7769
hanna244 <shhn0509@gmail.com>
2021-02-26 12:09:24 +0900

    [실습] 해당 커밋까지 순수 Redux 사용한 공부

--- __PRACTICE__/react_redux/src/App.js
+++ __PRACTICE__/react_redux/src/App.js
@@ -10,15 +10,11 @@ function App() {
   const combindClassNames = classNames('App-logo', animationClass)
 
   const [control, setControl] = useState(true)
-
-  //${!control ? '실행' : '정지'}가 중복 사용 되기 때문에 유틸리티 함수 사용
   const controlText = () => (!control ? '실행' : '정지')
 
   const animationStateChange = () => {
-    // 3항식 사용하지 않고 부정 연산자로 토글 사용 가능
     setControl(!control)
     store.dispatch({
-      // type: `로고 애니메이션 ${!control ? '실행' : '정지'}`,
       type: `로고 애니메이션 ${controlText()}`,
     })
   }
@@ -27,7 +23,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className={combindClassNames} alt="logo" />
-        {/* 중복 되는 AppButton 컴포넌트를 두 번 사용하지 않고 아래와 같이 사용할 수 있음. */}
         <AppButton
           children={`로고 애니메이션 ${controlText()}`}
           onClick={animationStateChange}
