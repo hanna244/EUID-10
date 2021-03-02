fb0704f7a5e23d7e5fb5e50296d372cd7781cb92
hanna244 <shhn0509@gmail.com>
2021-03-02 14:33:50 +0900

    [추가] 스타일 컴포넌트 주석 추가
    컨테이너 컴포넌트에 스타일 컴포넌트를 사용할 때 Sass의 믹스인 처럼
    재사용할 수 있다는 것을 배웠다.

--- __OFFLINE__/20210227/src/components/Container/Container.js
+++ __OFFLINE__/20210227/src/components/Container/Container.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/macro'
 
+// Sass 믹스인 처럼 재사용할 수 있는 믹스인이다.
 const Container = styled.div`
   max-width: ${({ max }) => max ?? '1280px'};
   min-width: ${({ min }) => min ?? '480px'};
