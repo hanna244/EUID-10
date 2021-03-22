23dc20873e53a65c7603811f20a2b79372d3c579
hanna244 <shhn0509@gmail.com>
2021-03-22 19:57:03 +0900

    [스프링] 사용자 정의 useSpring

--- __PRACTICE__/react-spring/src/App.js
+++ __PRACTICE__/react-spring/src/App.js
@@ -5,14 +5,44 @@ import './App.css'
 import { ToggleButton } from './ToggleButton/ToggleButton'
 
 function App() {
-  const [toggle, setToggle] = React.useState(false)
+  const [toggle, setToggle] = React.useState(true)
   const showLogo = useSpring({
     opacity: toggle ? 1 : 0,
   })
 
-  const innerHtmlNumber = useSpring({
-    number: 3,
-    from: { number: 0 },
+  /* innerHtmlNumber ----------- */
+  // const innerHtmlNumber = useSpring({
+  //   to: { number: 3 },
+  //   from: { number: 0 },
+  // })
+
+  // const props = useSpring({
+  //   to: { opacity: 1, color: '#fff' },
+  //   from: { color: '#73e77f' },
+  //   delay: 1000,
+  // })
+
+  /* 사용자 정의 useSpring --------------------------------------------------------- */
+
+  /* 방법 1 ----------- */
+
+  // const props = useSpring({
+  //   to: async (next, cancel) => {
+  //     await next({ opacity: 1, color: '#ffaaee' })
+  //     await next({ opacity: 0.5, color: '#73e77f' })
+  //     await next({ opacity: 0, color: '#fbea2a' })
+  //   },
+  //   from: { opacity: 0, color: 'red' },
+  // })
+
+  /* 방법 1 ----------- */
+  const props = useSpring({
+    to: [
+      { opacity: 1, color: '#ffaaee' },
+      { opacity: 0.5, color: '#73e77f' },
+      { opacity: 0, color: '#fbea2a' },
+    ],
+    from: { opacity: 0, color: 'red' },
   })
 
   return (
@@ -24,7 +54,10 @@ function App() {
           className="App-logo"
           alt="logo"
         />
-        <animated.span>{innerHtmlNumber.number}</animated.span>
+        <br />
+        {/* <animated.span style={props}>{innerHtmlNumber.number}</animated.span> */}
+        <animated.span style={props}>안녕하세요!</animated.span>
+        <br />
         <ToggleButton handle={() => setToggle(!toggle)} />
       </header>
     </div>
