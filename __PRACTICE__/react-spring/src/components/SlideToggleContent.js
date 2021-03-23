eac4d9339693f8ea7d7b0dcc3982e17c1f1ae9f6
hanna244 <shhn0509@gmail.com>
2021-03-23 20:12:05 +0900

    [스프링] 이전의 문제 해결, 열고 닫기 성공

--- __PRACTICE__/react-spring/src/components/SlideToggleContent.js
+++ __PRACTICE__/react-spring/src/components/SlideToggleContent.js
@@ -1,9 +1,18 @@
 import { useTransition, animated } from 'react-spring'
 import React from 'react'
 
+const visibleStyle = { opacity: 1 }
+const hiddenStyle = { opacity: 0 }
+
 export const SlideToggleContent = ({ isVisible, children, ...restProps }) => {
   const transition = useTransition(isVisible, null, {
-    enter: {},
+    enter: () => async (next, cancel) => {
+      await next(visibleStyle)
+    },
+    leave: () => async (next, calcel) => {
+      await next(hiddenStyle)
+    },
+    from: isVisible ? visibleStyle : hiddenStyle,
   })
 
   return transition.map(({ item: show, prop: toggleStyle, key }) =>
